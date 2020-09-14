32ca85566eab3893ade7b4e4cf4f4789a7e48881
Shreeya <shreeyabhuwalka@gmail.com>
2020-09-14 08:11:26 +0530

    Initial commit

---
+++ app/src/main/java/com/developer/toolbar/MainActivity.java
@@ -0,0 +1,18 @@
+package com.developer.toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
